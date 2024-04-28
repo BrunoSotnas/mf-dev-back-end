b45e3ed6ec3592792dac3dfd343b0d68716c672e
Bruno Santos <110935208+BrunoSotnas@users.noreply.github.com>
2024-04-28 16:52:32 +0100

    Configuração do Banco de Dados e da Tabela veiculos

---
+++ mf-dev-back-end/Models/veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_back_end.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a Placa")]
+        public string Placa { get; set;}
+        
+        [Required(ErrorMessage = "Obrigatório informar o Ano da Fabricação")]
+
+        public string AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo")]
+
+        public string AnoModelo { get; set; }
+
+    }
+}
--- mf-dev-back-end/Program.cs
+++ mf-dev-back-end/Program.cs
@@ -1,3 +1,6 @@
+using mf_dev_back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace mf_dev_back_end
 {
     public class Program
@@ -10,6 +13,10 @@ namespace mf_dev_back_end
             builder.Services.AddControllersWithViews();
             
             builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             var app = builder.Build();
 
